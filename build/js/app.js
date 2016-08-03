54a265eb8b88bf7c0dec4c5a35f1ed5dd20fc213
Bhawani Parajuli and Kyle Helyar <kyle@>
2016-08-03 11:40:48 -0700

    add source for gmap api to js

--- build/js/app.js
+++ build/js/app.js
@@ -3,8 +3,25 @@ var apiKey = "AIzaSyDTxxSTai-Jb7jEh6w4_euhPozoiFTh89M";
 
 },{}],2:[function(require,module,exports){
 var apiKey = require("./../.env").apiKey;
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+
 $(document).ready(function(){
-  
+
+   $.get('https://maps.googleapis.com/maps/api/js?key='+ apiKey + '&callback=initMap',
+   function(newMap){
+     $('#map').text(new google.maps.Map(document.getElementById('map'), {
+       center: {lat: -34.397, lng: 150.644},
+       zoom: 8
+     }));
+   });
+
 });
 
 },{"./../.env":1}]},{},[2]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,4 +1,21 @@
 var apiKey = require("./../.env").apiKey;
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+
 $(document).ready(function(){
-  
+
+   $.get('https://maps.googleapis.com/maps/api/js?key='+ apiKey + '&callback=initMap',
+   function(newMap){
+     $('#map').text(new google.maps.Map(document.getElementById('map'), {
+       center: {lat: -34.397, lng: 150.644},
+       zoom: 8
+     }));
+   });
+
 });
