f382f0afb1de104c1e09d0718959f04ce9bcd3db
Bhawani Parajuli and Kyle Helyar <bhawani@>
2016-08-03 14:13:43 -0700

    add markers on the map

--- build/js/app.js
+++ build/js/app.js
@@ -4,22 +4,46 @@ var apiKey = "AIzaSyDTxxSTai-Jb7jEh6w4_euhPozoiFTh89M";
 },{}],2:[function(require,module,exports){
 var apiKey = require("./../.env").apiKey;
 
+var markers = [];
 var map;
 function initMap() {
+  var myLatLng = {lat: 37.0902, lng: -95.7129};
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 37.0902, lng: -95.7129},
+    zoom: 5
   });
+
+  function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(map);
+    }
+  }
+
+  function clearMarkers() {
+    setMapOnAll(null);
+  }
+
+  function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+  }
+
+  map.addListener('click', function(event) {
+
+    var marker = new google.maps.Marker({position: event.latLng, map: map});
+    markers.push(marker);
+    console.log(markers);
+  });
+
+
+
 }
 
-$(document).ready(function(){
 
+$(document).ready(function(){
    $.get('https://maps.googleapis.com/maps/api/js?key='+ apiKey + '&callback=initMap',
    function(newMap){
-     $('#map').text(new google.maps.Map(document.getElementById('map'), {
-       center: {lat: -34.397, lng: 150.644},
-       zoom: 8
-     }));
+     $("#map").show(initMap());
    });
 
 });
