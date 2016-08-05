b64ca1cb7d1d14cad0a6dc7dd611810f771d0ef0
Kyle Helyar <helyar@live.ca>
2016-08-05 10:45:45 -0700

    displays list of users repos

--- build/js/app.js
+++ build/js/app.js
@@ -5,10 +5,11 @@ exports.apiKey = "1058a1624bc04975fc3d59ecad0d21584bb807b5";
 var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(userName){
-  $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
+  $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(info){
+console.log(info);
+    for (var i=0; i<info.length; i++){
+      $('#repos').append('<p>' + info[i].name + '</p>');
+    }
   });
 };
 
--- js/search.js
+++ js/search.js
@@ -1,9 +1,10 @@
 var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(userName){
-  $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
+  $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(info){
+console.log(info);
+    for (var i=0; i<info.length; i++){
+      $('#repos').append('<p>' + info[i].name + '</p>');
+    }
   });
 };
